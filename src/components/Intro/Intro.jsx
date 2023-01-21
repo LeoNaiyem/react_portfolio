781f8a52927a249833b259147d462e8487c343e2
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2023-01-21 21:57:47 +0600

    added some links

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -7,10 +7,10 @@ import Github from "../../img/github.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import Instagram from "../../img/instagram.png";
 import LinkedIn from "../../img/linkedin.png";
-import boy from "../../img/naiyem-removebg-preview.png";
+import boy from "../../img/naiyem portfolio.png";
 import thumbup from "../../img/thumbup.png";
-import Vector1 from "../../img/Vector1.png";
-import Vector2 from "../../img/Vector2.png";
+// import Vector1 from "../../img/Vector1.png";
+// import Vector2 from "../../img/Vector2.png";
 import FloatinDiv from "../FloatingDiv/FloatingDiv";
 import "./Intro.css";
 const Intro = () => {
@@ -27,11 +27,11 @@ const Intro = () => {
       <div className="i-left">
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
-          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span style={{ color: darkMode ? "white" : "" }}>Hi! I Am</span>
           <span>Naiyem Hossain</span>
           <span>
             Frontend Developer with high level of experience in web designing
-            and development, producting the Quality work
+            and development, producing the Quality work
           </span>
         </div>
         <Link to="contact" smooth={true} spy={true}>
@@ -39,15 +39,36 @@ const Intro = () => {
         </Link>
         {/* social icons */}
         <div className="i-icons">
-          <img src={Github} alt="" />
+          <a
+            href="https://github.com/leonaiyem"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Github} alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/naiyem-hossain-a94b961a2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           <img src={LinkedIn} alt="" />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100005953965379"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           <img src={Instagram} alt="" />
+          </a>
+          
         </div>
       </div>
       {/* right image side */}
       <div className="i-right">
-        <img src={Vector1} alt="" />
-        <img src={Vector2} alt="" />
+        <span></span>
+        <span></span>
+        {/* <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" /> */}
         <img src={boy} alt="" />
         {/* animation */}
         <motion.img
